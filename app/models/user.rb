53753f80e2e3f56a6691c604b7195a8cb59b24ea
rib-pietro <ribpietro@gmail.com>
2018-08-19 13:12:11 -0300

    relações entre usuários

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followers, through: :passive_relationships, source: :follower
+
   has_many :tweets
+
+  def follow(id_following)
+  	@following = User.find(id_following)
+  	@rel = Relationship.new
+  	@rel.follower = self
+  	@rel.followed = @following
+  end
+
 end
